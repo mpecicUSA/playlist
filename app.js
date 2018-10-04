37b378972f4ae47cc3aa89e4483a1ee5fd78f95f
Marko Pecic <mpecic2010@gmail.com>
2018-10-04 15:04:54 -0700

    added

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("app.js is connected")
+const albumURL = "https://lit-fortress-6467.herokuapp.com/object"
+const album = document.querySelector(".albums");
+
+axios.get(albumURL).then(res => {
+  for(let i=0; i< 4; i++){
+    let newDiv = document.createElement("div");
+    newDiv.class = "generatedDiv"
+    let newImg = document.createElement('img');
+    newImg.src = `images/${res.data.results[i].cover_art}`;
+    album.appendChild(newDiv)
+    newDiv.appendChild(newImg);
+  }
+});
