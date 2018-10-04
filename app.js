4b5564d92a21bb111d9ebc8ecca6130451892e04
Marko Pecic <mpecic2010@gmail.com>
2018-10-04 15:40:04 -0700

    html css happy, random generated photos

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const albumURL = "https://lit-fortress-6467.herokuapp.com/object"
 const album = document.querySelector(".albums");
 
 axios.get(albumURL).then(res => {
-  for(let i=0; i< 4; i++){
+  console.log(res);
+  function randomNumber() {
+    return Math.floor(Math.random()*5)
+  };
+
+  for(let i=0; i<3; i++){
     let newDiv = document.createElement("div");
-    newDiv.class = "generatedDiv"
+    newDiv.className = "generatedDiv"
     let newImg = document.createElement('img');
-    newImg.src = `images/${res.data.results[i].cover_art}`;
+    newImg.src = `images/${res.data.results[randomNumber()].cover_art}`;
     album.appendChild(newDiv)
     newDiv.appendChild(newImg);
   }
