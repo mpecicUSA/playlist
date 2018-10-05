9cd49524cb6ceb534e0cc60468288b24e297a7bd
Marko Pecic <mpecic2010@gmail.com>
2018-10-04 19:33:40 -0700

    pull album artwork from api in scrolling div

--- playListApp.js
+++ playListApp.js
@@ -1,1 +1,14 @@
-console.log("JS is linked")
+const albumURL = "https://lit-fortress-6467.herokuapp.com/object"
+const albumHolder = document.querySelector(".container-inner");
+
+axios.get(albumURL).then(res => {
+  console.log(res);
+  for(let i=0; i<5; i++){
+    let newDiv = document.createElement("div");
+    newDiv.className = "generatedDiv"
+    let newImg = document.createElement('img');
+    newImg.src = `images/${res.data.results[i].cover_art}`;
+    albumHolder.appendChild(newDiv)
+    newDiv.appendChild(newImg);
+  }
+});
